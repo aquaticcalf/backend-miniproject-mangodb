c69b574f5a6bf8e13d707e82c5a9f1a128b703d3
sam <aquaticcalf@proton.me>
2024-03-19 11:22:24 +0530

    meow

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const { User } = require('../models')
+
+const auth = async ( request, response, next ) => {
+	const token = request.header('Authorization')
+
+	if( !token ) {
+		return response.status(401).json({ message : 'authorization denied' })
+	}
+
+	try {
+		const decoded = jwt.verify( token, 'secret_key' )
+		const user = await User.findById(decoded.userId)
+
+		if (!user) {
+			return response.status(401).json({ message : 'invalid token' })
+		}
+
+		request.user = user
+		next()
+	}
+
+	catch (error) {
+		response.status(400).json({ message : 'invalid token' })
+	}
+}
+
+module.exports = auth
---
+++ routes/profile.js
@@ -0,0 +1,96 @@
+const router = require('express').Router()
+const { Post, Comment, User, Tag } = require('../models')
+const auth = require('../middleware/auth')
+
+// self profile
+router.get('/', auth, async (request, response) => {
+    try {
+        const username = request.user.username
+
+        const user = await User.findOne({ username })
+            .populate({ path: 'posts', populate: [{ path: 'comments' }, { path: 'tags' }] })
+            .populate('comments')
+
+        if (!user) {
+            return response.status(404).json({ message: 'user not found' })
+        }
+
+        const userProfile = {
+            id: user._id,
+            username: user.username,
+            posts: user.posts.map((post) => ({
+                id: post._id,
+                title: post.title,
+                content: post.content,
+                tags: post.tags.map((tag) => ({
+                    id: tag._id,
+                    tag: tag.name,
+                })),
+                comments: post.comments.map((comment) => ({
+                    id: comment._id,
+                    content: comment.content,
+                    userId: comment.userId,
+                })),
+            })),
+            comments: user.comments.map((comment) => ({
+                id: comment._id,
+                content: comment.content,
+                postId: comment.postId,
+            })),
+        }
+
+        response.json(userProfile)
+    }
+    
+    catch (error) {
+      console.log(error)
+      response.status(500).json({ message: 'error' })
+    }
+  })
+  
+  // others profile
+  router.get('/:username', async (request, response) => {
+    try {
+        const username = request.params.username
+
+        const user = await User.findOne({ username })
+            .populate({ path: 'posts', populate: [{ path: 'comments' }, { path: 'tags' }] })
+            .populate('comments')
+
+        if (!user) {
+            return response.status(404).json({ message: 'user not found' })
+        }
+
+        const userProfile = {
+            id: user._id,
+            username: user.username,
+            posts: user.posts.map((post) => ({
+                id: post._id,
+                title: post.title,
+                content: post.content,
+                tags: post.tags.map((tag) => ({
+                    id: tag._id,
+                    tag: tag.name,
+                })),
+                comments: post.comments.map((comment) => ({
+                        id: comment._id,
+                        content: comment.content,
+                        userId: comment.userId,
+                })),
+            })),
+            comments: user.comments.map((comment) => ({
+                id: comment._id,
+                content: comment.content,
+                postId: comment.postId,
+            })),
+        }
+
+        response.json(userProfile)
+    } 
+    
+    catch (error) {
+        console.log(error)
+        response.status(500).json({ message: 'error' })
+    }
+  })
+  
